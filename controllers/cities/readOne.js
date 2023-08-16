908bf22077786984ed03ae514e89921415e2b174
CrisRonda10 <100365960+CrisRonda10@users.noreply.github.com>
2023-08-16 12:03:16 -0300

    Middlewares (#5)
    * controllers de cities y users + manejo de errores
    
    * probando nuevo pull request

--- controllers/cities/readOne.js
+++ controllers/cities/readOne.js
@@ -1,18 +1,23 @@
 import City from "../../models/City.js";
-export default async (req, res) => {
+
+export default async (req, res, next) => {
     try {
         let oneCity = await City.findOne({_id:req.params.id}).select("country photo city")
         // let oneCityId = await City.findById(req.params.id)
-        return res.status(200).json({
-            success: true,
-            message: 'city found',
-            response: oneCity
-        })
+        if (oneCity){
+            return res.status(200).json({
+                success: true,
+                message: 'city found',
+                response: oneCity
+            })
+        }else{
+            return res.status(400).json({
+                success: false,
+                message: 'not found',
+                response: null
+            })
+        }
     } catch (error) {
-        return res.status(400).json({
-            success: false,
-            message: 'not found',
-            response: null
-        })
+        next(error)
     }
 }
